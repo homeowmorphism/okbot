be7be1de5627247ea3f6f6bf9beb81741d36d139
homeowmorphism <homeowmorphism@gmail.com>
2017-07-18 15:56:53 -0400

    testing ignore

--- creds.py
+++ creds.py
@@ -1,4 +1,4 @@
-consumer_key = #'''paste consumer key here, for example '1234567800jhstuey23486'
+consumer_key = #''paste consumer key here, for example '1234567800jhstuey23486'
 consumer_secret = #paste consumer secret here, for example '1234567800jhstuey23486'
 access_token = #paste access token here, for example '1234567800jhstuey23486'
 access_token_secret = #paste access token secret here, for example '1234567800jhstuey23486'
